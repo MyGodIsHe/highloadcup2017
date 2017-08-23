3d00abc65a9403ceda7359186ba76769b47f0a81
Ilya Chistyakov <ilchistyakov@gmail.com>
2017-08-23 11:48:05 +0700

    go 1.8.3 and custom router

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,19 @@
+#FROM centos:7
+#WORKDIR /root
+#ADD golang golang
+#EXPOSE 80
+#CMD ./golang
+
 FROM centos:7
 WORKDIR /root
-ADD traveler traveler
+RUN yum install -y wget && \
+    wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+RUN tar -C /usr/local -xzf go1.8.3.linux-amd64.tar.gz && \
+    mkdir go && mkdir go/src && mkdir go/bin && mkdir go/pkg && \
+    mkdir go/src/dumb
+RUN yum install -y git
+ENV PATH=${PATH}:/usr/local/go/bin GOROOT=/usr/local/go GOPATH=/root/go
+ADD *.go go/src/traveler/
+RUN go get traveler && go build traveler && go install traveler
 EXPOSE 80
-CMD ./traveler
+CMD ./go/bin/traveler
--- main.go
+++ main.go
@@ -17,342 +17,405 @@ import (
 	"strconv"
 	"fmt"
 	"log"
-
-	"github.com/buaazp/fasthttprouter"
-	"github.com/valyala/fasthttp"
 	"bytes"
+
+	"github.com/valyala/fasthttp"
 )
+
+func users_get(ctx *fasthttp.RequestCtx, id int) {
+	rec, ok := users[id]
+	if !ok {
+		ctx.SetStatusCode(404)
+		return
+	}
+	rec.Write(ctx)
+}
+func users_create(ctx *fasthttp.RequestCtx) {
+	body := ctx.PostBody()
+	if len(body) == 0 || bytes.Contains(body, NULL) {
+		ctx.SetStatusCode(400)
+		ctx.SetConnectionClose()
+	}
+	ctx.Write(OK)
+	ctx.SetConnectionClose()
+
+	var rec User
+
+	if !rec.Update(body, true) {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	_, ok := users_emails[rec.Email]
+	if ok {
+		ctx.SetStatusCode(400)
+		return
+	}
+	users_emails[rec.Email] = true
+	users[rec.Id] = rec
+}
+
+func users_update(ctx *fasthttp.RequestCtx, id int) {
+	body := ctx.PostBody()
+	if len(body) == 0 || bytes.Contains(body, NULL) {
+		ctx.SetStatusCode(400)
+		ctx.SetConnectionClose()
+		return
+	}
+	ctx.Write(OK)
+	ctx.SetConnectionClose()
+
+	var rec User
+
+	var ok bool
+	rec, ok = users[id]
+	if !ok {
+		ctx.SetStatusCode(404)
+		return
+	}
+
+	old_email := rec.Email
+
+	if !rec.Update(body, false) {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	if old_email != rec.Email {
+		_, ok := users_emails[rec.Email]
+		if ok {
+			ctx.SetStatusCode(400)
+			return
+		}
+
+		delete(users_emails, old_email)
+		users_emails[rec.Email] = true
+	}
+	users[rec.Id] = rec
+}
+
+func locations_get(ctx *fasthttp.RequestCtx, id int) {
+	rec, ok := locations[id]
+	if !ok {
+		ctx.SetStatusCode(404)
+		return
+	}
+	rec.Write(ctx)
+}
+
+func locations_create(ctx *fasthttp.RequestCtx) {
+	body := ctx.PostBody()
+	if len(body) == 0 || bytes.Contains(body, NULL) {
+		ctx.SetStatusCode(400)
+		ctx.SetConnectionClose()
+		return
+	}
+
+	ctx.Write(OK)
+	ctx.SetConnectionClose()
+
+	var rec Location
+	if !rec.Update(body, true) {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	locations[rec.Id] = rec
+}
+
+func locations_update(ctx *fasthttp.RequestCtx, id int) {
+	body := ctx.PostBody()
+	if len(body) == 0 || bytes.Contains(body, NULL) {
+		ctx.SetStatusCode(400)
+		ctx.SetConnectionClose()
+		return
+	}
+
+	ctx.Write(OK)
+	ctx.SetConnectionClose()
+
+	var rec Location
+	var ok bool
+	rec, ok = locations[id]
+	if !ok {
+		ctx.SetStatusCode(404)
+		return
+	}
+
+	if !rec.Update(body, false) {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	locations[rec.Id] = rec
+}
+
+func visits_get(ctx *fasthttp.RequestCtx, id int) {
+	rec, ok := visits[id]
+	if !ok {
+		ctx.SetStatusCode(404)
+		return
+	}
+	rec.Write(ctx)
+}
+
+func visits_create(ctx *fasthttp.RequestCtx) {
+	body := ctx.PostBody()
+	if len(body) == 0 || bytes.Contains(body, NULL) {
+		ctx.SetStatusCode(400)
+		ctx.SetConnectionClose()
+		return
+	}
+
+	ctx.Write(OK)
+	ctx.SetConnectionClose()
+
+	var rec Visit
+	if !rec.Update(body, true) {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	visitSetEvent(rec)
+}
+
+func visits_update(ctx *fasthttp.RequestCtx, id int) {
+	body := ctx.PostBody()
+	if len(body) == 0 || bytes.Contains(body, NULL) {
+		ctx.SetStatusCode(400)
+		ctx.SetConnectionClose()
+		return
+	}
+
+	ctx.Write(OK)
+	ctx.SetConnectionClose()
+
+	var rec Visit
+	var ok bool
+	rec, ok = visits[id]
+	if !ok {
+		ctx.SetStatusCode(404)
+		return
+	}
+
+	if !rec.Update(body, false) {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	visitSetEvent(rec)
+}
+
+func users_visits(ctx *fasthttp.RequestCtx, id int) {
+	var err interface{}
+
+	_, ok := users[id]
+	if !ok {
+		ctx.SetStatusCode(404)
+		return
+	}
+
+	hasFromDate, fromDateValue, err := getIntFromQuery(ctx,"fromDate")
+	if err != nil {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	hasToDate, toDateValue, err := getIntFromQuery(ctx, "toDate")
+	if err != nil {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	country := string(ctx.URI().QueryArgs().Peek("country"))
+	var l Location
+
+	ctx.URI().QueryArgs().GetUintOrZero("toDistance")
+	hasToDistance, toDistanceValue, err := getIntFromQuery(ctx, "toDistance")
+	if err != nil {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	result := ShortVisits{}
+	for _, v := range visits_by_user[id] {
+		if hasFromDate && v.VisitedAt <= fromDateValue {
+			continue
+		}
+		if hasToDate && v.VisitedAt >= toDateValue {
+			continue
+		}
+		l = locations[v.Location]
+		if country != "" && l.Country != country {
+			continue
+		}
+		if hasToDistance && l.Distance >= toDistanceValue {
+			continue
+		}
+		result = append(result, ShortVisit{Mark: v.Mark, Place: l.Place, VisitedAt: v.VisitedAt})
+	}
+	sort.Sort(result)
+	WriteShortVisits(ctx, result)
+	//json.NewEncoder(ctx).Encode(DataShortVisit{Visits: result})
+}
+
+func locations_avg(ctx *fasthttp.RequestCtx, id int) {
+	var err interface{}
+	_, ok := locations[id]
+	if !ok {
+		ctx.SetStatusCode(404)
+		return
+	}
+
+
+	hasFromDate, fromDateValue, err := getIntFromQuery(ctx, "fromDate")
+	if err != nil {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	hasToDate, toDateValue, err := getIntFromQuery(ctx, "toDate")
+	if err != nil {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	hasFromAge, fromAgeValue, err := getIntFromQuery(ctx, "fromAge")
+	if err != nil {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	hasToAge, toAgeValue, err := getIntFromQuery(ctx, "toAge")
+	if err != nil {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	gender := string(ctx.URI().QueryArgs().Peek("gender"))
+	if gender != "" && gender != "f" && gender != "m" {
+		ctx.SetStatusCode(400)
+		return
+	}
+
+	now := time.Now().UTC()
+
+	avgCount := 0
+	avgSum := 0
+	for _, v := range visits_by_location[id] {
+		if hasFromDate && v.VisitedAt <= fromDateValue {
+			continue
+		}
+		if hasToDate && v.VisitedAt >= toDateValue {
+			continue
+		}
+		u := users[v.User]
+		if gender != "" && u.Gender != gender {
+			continue
+		}
+		age := diff(time.Unix(int64(u.BirthDate), 0).UTC(), now)
+		if hasFromAge && age < fromAgeValue {
+			continue
+		}
+		if hasToAge && age >= toAgeValue {
+			continue
+		}
+		avgCount++
+		avgSum += v.Mark
+	}
+	var avg float64
+	if avgCount != 0 {
+		avg = float64(avgSum) / float64(avgCount)
+	}
+	//avg, _ = strconv.ParseFloat(fmt.Sprintf("%.5f", avg), 64)
+	//avg = float32(int32(avg*100000+0.5)) / 100000
+	WriteAvg(ctx, avg)
+}
+
+func RouterHandler(ctx *fasthttp.RequestCtx) {
+	path := string(ctx.Path())
+	pl := len(path) - 1
+	method := ctx.Method()
+
+	if method[0] == 'G' {
+		if path[1] == 'u' {
+			if path[pl] == 's' {
+				id, _ := strconv.Atoi(path[7:pl-6])
+				users_visits(ctx, id)
+				return
+			}
+			id, _ := strconv.Atoi(path[7:])
+			users_get(ctx, id)
+			return
+		}
+		if path[1] == 'l' {
+			if path[pl] == 'g' {
+				id, _ := strconv.Atoi(path[11:pl-3])
+				locations_avg(ctx, id)
+				return
+			}
+			id, _ := strconv.Atoi(path[11:])
+			locations_get(ctx, id)
+			return
+		}
+		if path[1] == 'v' {
+			id, _ := strconv.Atoi(path[8:])
+			visits_get(ctx, id)
+			return
+		}
+	} else {
+		if path[1] == 'u' {
+			if path[7] == 'n' {
+				users_create(ctx)
+				return
+			}
+			id, _ := strconv.Atoi(path[7:])
+			users_update(ctx, id)
+			return
+		}
+		if path[1] == 'l' {
+			if path[11] == 'n' {
+				locations_create(ctx)
+				return
+			}
+			id, _ := strconv.Atoi(path[11:])
+			locations_update(ctx, id)
+			return
+		}
+		if path[1] == 'v' {
+			if path[8] == 'n' {
+				visits_create(ctx)
+				return
+			}
+			id, _ := strconv.Atoi(path[8:])
+			visits_update(ctx, id)
+			return
+		}
+	}
+
+	ctx.Error(fasthttp.StatusMessage(fasthttp.StatusNotFound),
+		fasthttp.StatusNotFound)
+}
+
 
 
 func main() {
 	loadData("/tmp/data/data.zip")
 
-	router := fasthttprouter.New()
-
-	router.GET("/users/:id", func(ctx *fasthttp.RequestCtx) {
-		id, err := strconv.Atoi(ctx.UserValue("id").(string))
-		if err != nil {
-			ctx.SetStatusCode(404)
-			return
-		}
-		rec, ok := users[id]
-		if !ok {
-			ctx.SetStatusCode(404)
-			return
-		}
-		rec.Write(ctx)
-	})
-
-	router.POST("/users/:id", func(ctx *fasthttp.RequestCtx) {
-		body := ctx.PostBody()
-		if len(body) == 0 || bytes.Contains(body, NULL) {
-			ctx.SetStatusCode(400)
-			ctx.SetConnectionClose()
-			return
-		}
-		ctx.Write(OK)
-		ctx.SetConnectionClose()
-
-		var id int
-		var err interface{}
-		var rec User
-
-		param := ctx.UserValue("id").(string)
-		is_insert := param == "new"
-		if !is_insert {
-			id, err = strconv.Atoi(param)
-			if err != nil {
-				ctx.SetStatusCode(404)
-				return
-			}
-			var ok bool
-			rec, ok = users[id]
-			if !ok {
-				ctx.SetStatusCode(404)
-				return
-			}
-		}
-
-		old_email := rec.Email
-
-		if !rec.Update(body, is_insert) {
-			ctx.SetStatusCode(400)
-			return
-		}
-
-		if is_insert {
-			_, ok := users_emails[rec.Email]
-			if ok {
-				ctx.SetStatusCode(400)
-				return
-			}
-			users_emails[rec.Email] = true
-		} else {
-			if old_email != rec.Email {
-				_, ok := users_emails[rec.Email]
-				if ok {
-					ctx.SetStatusCode(400)
-					return
-				}
-
-				delete(users_emails, old_email)
-				users_emails[rec.Email] = true
-			}
-		}
-		users[rec.Id] = rec
-	})
-
-	router.GET("/locations/:id", func(ctx *fasthttp.RequestCtx) {
-		id, err := strconv.Atoi(ctx.UserValue("id").(string))
-		if err != nil {
-			ctx.SetStatusCode(404)
-			return
-		}
-		rec, ok := locations[id]
-		if !ok {
-			ctx.SetStatusCode(404)
-			return
-		}
-		rec.Write(ctx)
-	})
-
-	router.POST("/locations/:id", func(ctx *fasthttp.RequestCtx) {
-		body := ctx.PostBody()
-		if len(body) == 0 || bytes.Contains(body, NULL) {
-			ctx.SetStatusCode(400)
-			ctx.SetConnectionClose()
-			return
-		}
-
-		ctx.Write(OK)
-		ctx.SetConnectionClose()
-
-		var id int
-		var err interface{}
-		var rec Location
-
-		param := ctx.UserValue("id").(string)
-		is_insert := param == "new"
-		if !is_insert {
-			id, err = strconv.Atoi(param)
-			if err != nil {
-				ctx.SetStatusCode(404)
-				return
-			}
-			var ok bool
-			rec, ok = locations[id]
-			if !ok {
-				ctx.SetStatusCode(404)
-				return
-			}
-		}
-
-		if !rec.Update(body, is_insert) {
-			ctx.SetStatusCode(400)
-			return
-		}
-
-		locations[rec.Id] = rec
-	})
-
-	router.GET("/visits/:id", func(ctx *fasthttp.RequestCtx) {
-		id, err := strconv.Atoi(ctx.UserValue("id").(string))
-		if err != nil {
-			ctx.SetStatusCode(404)
-			return
-		}
-		rec, ok := visits[id]
-		if !ok {
-			ctx.SetStatusCode(404)
-			return
-		}
-		rec.Write(ctx)
-	})
-
-	router.POST("/visits/:id", func(ctx *fasthttp.RequestCtx) {
-		body := ctx.PostBody()
-		if len(body) == 0 || bytes.Contains(body, NULL) {
-			ctx.SetStatusCode(400)
-			ctx.SetConnectionClose()
-			return
-		}
-
-		ctx.Write(OK)
-		ctx.SetConnectionClose()
-
-		var id int
-		var err interface{}
-		var rec Visit
-
-		param := ctx.UserValue("id").(string)
-		is_insert := param == "new"
-		if !is_insert {
-			id, err = strconv.Atoi(param)
-			if err != nil {
-				ctx.SetStatusCode(404)
-				return
-			}
-			var ok bool
-			rec, ok = visits[id]
-			if !ok {
-				ctx.SetStatusCode(404)
-				return
-			}
-		}
-
-		if !rec.Update(body, is_insert) {
-			ctx.SetStatusCode(400)
-			return
-		}
-
-		visitSetEvent(rec)
-	})
-
-	router.GET("/users/:id/visits", func(ctx *fasthttp.RequestCtx) {
-		var id int
-		var err interface{}
-
-		id, err = strconv.Atoi(ctx.UserValue("id").(string))
-		if err != nil {
-			ctx.SetStatusCode(404)
-			return
-		}
-
-		_, ok := users[id]
-		if !ok {
-			ctx.SetStatusCode(404)
-			return
-		}
-
-		hasFromDate, fromDateValue, err := getIntFromQuery(ctx,"fromDate")
-		if err != nil {
-			ctx.SetStatusCode(400);
-			return
-		}
-
-		hasToDate, toDateValue, err := getIntFromQuery(ctx, "toDate")
-		if err != nil {
-			ctx.SetStatusCode(400);
-			return
-		}
-
-		country := string(ctx.URI().QueryArgs().Peek("country"))
-		var l Location
-
-		ctx.URI().QueryArgs().GetUintOrZero("toDistance")
-		hasToDistance, toDistanceValue, err := getIntFromQuery(ctx, "toDistance")
-		if err != nil {
-			ctx.SetStatusCode(400);
-			return
-		}
-
-		result := ShortVisits{}
-		for _, v := range visits_by_user[id] {
-			if hasFromDate && v.VisitedAt <= fromDateValue {
-				continue
-			}
-			if hasToDate && v.VisitedAt >= toDateValue {
-				continue
-			}
-			l = locations[v.Location]
-			if country != "" && l.Country != country {
-				continue
-			}
-			if hasToDistance && l.Distance >= toDistanceValue {
-				continue
-			}
-			result = append(result, ShortVisit{Mark: v.Mark, Place: l.Place, VisitedAt: v.VisitedAt})
-		}
-		sort.Sort(result)
-		WriteShortVisits(ctx, result)
-		//json.NewEncoder(ctx).Encode(DataShortVisit{Visits: result})
-	})
-
-	router.GET("/locations/:id/avg", func(ctx *fasthttp.RequestCtx) {
-		var id int
-		var err interface{}
-
-		id, err = strconv.Atoi(ctx.UserValue("id").(string))
-		if err != nil {
-			ctx.SetStatusCode(404)
-			return
-		}
-
-		_, ok := locations[id]
-		if !ok {
-			ctx.SetStatusCode(404)
-			return
-		}
-
-
-		hasFromDate, fromDateValue, err := getIntFromQuery(ctx, "fromDate")
-		if err != nil {
-			ctx.SetStatusCode(400);
-			return
-		}
-
-		hasToDate, toDateValue, err := getIntFromQuery(ctx, "toDate")
-		if err != nil {
-			ctx.SetStatusCode(400);
-			return
-		}
-
-		hasFromAge, fromAgeValue, err := getIntFromQuery(ctx, "fromAge")
-		if err != nil {
-			ctx.SetStatusCode(400);
-			return
-		}
-
-		hasToAge, toAgeValue, err := getIntFromQuery(ctx, "toAge")
-		if err != nil {
-			ctx.SetStatusCode(400);
-			return
-		}
-
-		gender := string(ctx.URI().QueryArgs().Peek("gender"))
-		if gender != "" && gender != "f" && gender != "m" {
-			ctx.SetStatusCode(400)
-			return
-		}
-
-		now := time.Now().UTC()
-
-		avgCount := 0
-		avgSum := 0
-		for _, v := range visits_by_location[id] {
-			if hasFromDate && v.VisitedAt <= fromDateValue {
-				continue
-			}
-			if hasToDate && v.VisitedAt >= toDateValue {
-				continue
-			}
-			u := users[v.User]
-			if gender != "" && u.Gender != gender {
-				continue
-			}
-			age := diff(time.Unix(int64(u.BirthDate), 0).UTC(), now)
-			if hasFromAge && age < fromAgeValue {
-				continue
-			}
-			if hasToAge && age >= toAgeValue {
-				continue
-			}
-			avgCount++
-			avgSum += v.Mark
-		}
-		var avg float64
-		if avgCount != 0 {
-			avg = float64(avgSum) / float64(avgCount)
-		}
-		//avg, _ = strconv.ParseFloat(fmt.Sprintf("%.5f", avg), 64)
-		//avg = float32(int32(avg*100000+0.5)) / 100000
-		WriteAvg(ctx, avg)
-	})
+	//router := fasthttprouter.New()
+	//router.GET("/users/:id", users_get)
+	//router.POST("/users/:id", users_update)
+	//router.GET("/locations/:id", locations_get)
+	//router.POST("/locations/:id", locations_update)
+	//router.GET("/visits/:id", visits_get)
+	//router.POST("/visits/:id", visits_update)
+	//router.GET("/users/:id/visits", users_visits)
+	//router.GET("/locations/:id/avg", locations_avg)
 
 	fmt.Println("Good luck ^-^")
 
 	server := fasthttp.Server{
-		Handler: router.Handler,
+		Handler: RouterHandler,
 	}
 	err := server.ListenAndServe(":80")
 	if err != nil {
